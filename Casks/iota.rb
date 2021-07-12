741490f75fc27ffc94c6105385b1cc27d413d17b
weiting <a26007565@gmail.com>
2021-07-12 15:47:37 +0000

    Update iota.rb

--- Casks/iota.rb
+++ Casks/iota.rb
@@ -1,13 +1,13 @@
-cask 'iota' do
-  version :latest
+cask "iota" do
+  version "1.12.2"
   sha256 :no_check
 
-  url 'https://gssweb.gss.com.tw/iota/iota.dmg',
+  url "https://gssweb.gss.com.tw/iota/iota.dmg",
       user_agent: :fake
-  name 'iota'
-  homepage 'https://im.gss.com.tw/'
+  name "iota"
+  homepage "https://im.gss.com.tw/"
 
   auto_updates true
 
-  app 'iota.app'
+  app "iota.app"
 end
