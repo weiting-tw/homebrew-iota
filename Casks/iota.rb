c10d968ee9d15a6a3f77dd3fe813ef03001f7465
weiting <a26007565@gmail.com>
2021-07-12 20:20:17 +0800

    Update iota production download path

--- Casks/iota.rb
+++ Casks/iota.rb
@@ -2,7 +2,7 @@ cask 'iota' do
   version :latest
   sha256 :no_check
 
-  url 'https://im.gss.com.tw/download/iota.dmg',
+  url 'https://gssweb.gss.com.tw/iota/iota.dmg',
       user_agent: :fake
   name 'iota'
   homepage 'https://im.gss.com.tw/'
