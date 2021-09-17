29756b983e1a4dc9ef80930a55bb1567325bdc62
weiting <a26007565@gmail.com>
2021-09-17 12:18:06 +0000

    Update iota.rb

--- Casks/iota.rb
+++ Casks/iota.rb
@@ -1,5 +1,5 @@
 cask "iota" do
-  version "1.12.2"
+  version "1.13.2"
   sha256 "323d4c8a9121bc1c33db8325f8d3a2d775e7c5f54e59fd8541f78a1bbfda4c8a"
 
   url "https://gssweb.gss.com.tw/iota/iota.dmg",
