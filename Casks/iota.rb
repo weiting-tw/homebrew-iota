a7f150334683dfda1a25ee32a45408925760efdf
weiting <a26007565@gmail.com>
2022-02-09 12:19:06 +0000

    chore: Update iota.rb

--- Casks/iota.rb
+++ Casks/iota.rb
@@ -1,6 +1,6 @@
 cask "iota" do
   version "1.14.1"
-  sha256 "11c94787a6c6a971aeed45b1ddf259b2e7ba807504261d2bdb34d5d5b3369810"
+  sha256 "75dc7158cfafaa8f94e68984c850bdfc254a5014cd6321608f46891827c51776"
 
   url "https://gssweb.gss.com.tw/iota/iota.dmg",
       user_agent: :fake
