01da1bae1f1a29b6365655457d8e38cdcecc0e8d
weiting <a26007565@gmail.com>
2022-08-28 10:36:07 +0800

    Update download link

--- Casks/iota.rb
+++ Casks/iota.rb
@@ -2,7 +2,7 @@ cask "iota" do
   version "1.16.1"
   sha256 "8153db54f6cb3447742c5d08e24bd5d7bbc17df5dec2fab231069fae7315de92"
 
-  url "https://gssweb.gss.com.tw/iota/iota.dmg",
+  url "https://im.gss.com.tw/download/iota.dmg",
       user_agent: :fake
   name "iota"
   homepage "https://im.gss.com.tw/"
