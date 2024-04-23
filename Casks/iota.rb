88f4aab824d4c83a02bd78e43d4294998568439a
weiting <a26007565@gmail.com>
2024-04-23 10:17:44 +0800

    Update prod download link

--- Casks/iota.rb
+++ Casks/iota.rb
@@ -2,10 +2,10 @@ cask "iota" do
   version "1.18.1"
   sha256 "819e202f3814979210b0bc3a2c0c96c2edda6efb8b555d8f0720b0ff7fea1549"
 
-  url "https://im.gss.com.tw/download/iota.dmg",
+  url "https://iotademo.gss.com.tw/download/iota.dmg",
       user_agent: :fake
   name "iota"
-  homepage "https://im.gss.com.tw/"
+  homepage "https://iotademo.gss.com.tw"
 
   app "iota.app"
 end
